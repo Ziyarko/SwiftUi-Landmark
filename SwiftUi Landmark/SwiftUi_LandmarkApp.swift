3355a16ab34b121be1f62b5febd9368fee5fba5d
Ziyarko <ziya.kok@hisarschool.k12.tr>
2024-11-20 14:18:06 +0300

    Initial Commit

---
+++ SwiftUi Landmark/SwiftUi_LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_LandmarkApp.swift
+//  SwiftUi Landmark
+//
+//  Created by Ziya Kok on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUi LandmarkTests/SwiftUi_LandmarkTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_LandmarkTests.swift
+//  SwiftUi LandmarkTests
+//
+//  Created by Ziya Kok on 20.11.2024.
+//
+
+import Testing
+@testable import SwiftUi_Landmark
+
+struct SwiftUi_LandmarkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
