ca1bf3e623e3bbfd2e1373e4c14fb033e28d73d1
Ziyarko <ziya.kok@hisarschool.k12.tr>
2024-11-26 21:13:14 +0300

    TGG

---
+++ SwiftUi Landmark/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  SwiftUi Landmark
+//
+//  Created by Ziya Kok on 20.11.2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
